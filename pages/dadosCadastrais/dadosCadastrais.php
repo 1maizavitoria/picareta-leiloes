e9e1893bc8d7943e040d5decc452a38d8507cf4b
abressam <amandabressam@gmail.com>
2023-04-03 00:03:04 -0300

    Adicionando método GET ao formulário conforme item 6 da rubrica

--- pages/dadosCadastrais/dadosCadastrais.php
+++ pages/dadosCadastrais/dadosCadastrais.php
@@ -16,67 +16,61 @@
     
     <div class="content">
 
-        <div class="left">
-            <?php
-            include './../../components/sidebar/sidebar.php';
-            ?>
-        </div>
-
         <div class="right">
 
             <div class="d-flex justify-content-center my-5">
                 <h2>Dados Cadastrais</h2>
             </div>
 
-            <form class="row d-flex justify-content-center" id="form" action="post">
+            <form class="row d-flex justify-content-center" id="form" method="GET" action="dadosCadastraisAction.php">
 
                 <div class="row justify-content-center mb-5">
                     <div class="col-6 col-lg-4">
-                        <input type="text" id="name" class="form-control" placeholder="Nome completo*" onblur="validateInput(this)" required>
+                        <input type="text" id="name" name="Nome completo" class="form-control" placeholder="Nome completo" onblur="validateInput(this)" required>
                         <div class="invalid-feedback" id="invalid-message-name">Informe um nome válido.<br> <em>Ex: Tyler Durden</em></div>
                     </div>
                     <div class="col-6 col-lg-4">
-                        <input type="email" id="email" class="form-control" placeholder="E-mail*" onblur="validateInput(this)" required>
+                        <input type="email" id="email" name="E-mail" class="form-control" placeholder="E-mail*" onblur="validateInput(this)" required>
                         <div class="invalid-feedback" id="invalid-message-email">Informe um e-mail válido.<br> <em>Ex: nome@dominio.com</em></div>
                     </div>
                 </div>
                 
                 <div class="row justify-content-center mb-5">
                     <div class="col-4 col-lg-3">
-                        <input type="text" id="cpf" maxLength="14" class="form-control" placeholder="CPF*" onblur="validateInput(this)" required>
+                        <input type="text" id="cpf" name="CPF" maxLength="14" class="form-control" placeholder="CPF*" onblur="validateInput(this)" required>
                         <div class="invalid-feedback" id="invalid-message-cpf">Informe um CPF válido.<br> <em>Ex: 123.456.789-00</em></div>
                     </div>
                     <div class="col-4 col-lg-3">
-                        <input type="text" id="rg" maxLength=12 class="form-control" placeholder="RG*" onblur="validateInput(this)" required>
+                        <input type="text" id="rg" name="RG" maxLength=12 class="form-control" placeholder="RG*" onblur="validateInput(this)" required>
                         <div class="invalid-feedback" id="invalid-message-rg">Informe um RG válido.<br> <em>Ex: 12.345.678-9</em></div>
                     </div>
                     <div class="col-4 col-lg-2">
-                        <input type="tel" maxLength="15" id="phone" class="form-control" placeholder="Celular*" onblur="validateInput(this)" required>
-                        <div class="invalid-feedback" id="invalid-message-phone">Informe um celular válido.<br> <em>Ex: (00) 90000-0000</em></div>
+                        <input type="tel" maxLength="15" id="phone" name="Celular" class="form-control" placeholder="Celular*" onblur="validateInput(this)" required>
+                        <div class="invalid-feedback" id="invalid-message-phone">Informe um celular válido.<br> <em>Ex: (11) 90000-0000</em></div>
                     </div>
                 </div>
 
                 <div class="row justify-content-center mb-5">
                     <div class="col-4 col-lg-3">
-                        <input type="date" id="birthDate" class="form-control" placeholder="Data de nascimento*" onblur="validateInput(this)" required>
+                        <input type="date" id="birthDate" name="Data de Nascimento" class="form-control" placeholder="Data de nascimento*" onblur="validateInput(this)" required>
                         <div class="invalid-feedback" id="invalid-message-birthDate">Informe uma data de nascimento válida.<br> <em>Ex: 01/01/2000</em></div>
                     </div>
                     <div class="col-5 col-lg-3">
-                        <select class="form-select" id="maritalStatus" onblur="validateInput(this)" required>
+                        <select class="form-select" id="maritalStatus" name="Estado Civil" onchange="validateInput(this)" required>
                             <option value="" disabled selected hidden>Estado civil*</option>
-                            <option value="1">Solteiro</option>
-                            <option value="2">Casado</option>
-                            <option value="3">Separado</option>
-                            <option value="4">Divorciado</option>
-                            <option value="5">Viúvo</option>
+                            <option value="Solteiro">Solteiro</option>
+                            <option value="Casado">Casado</option>
+                            <option value="Separado">Separado</option>
+                            <option value="Divorciado">Divorciado</option>
+                            <option value="Viúvo">Viúvo</option>
                         </select>
                         <div class="invalid-feedback" id="invalid-message-maritalStatus">Informe um estado civil válido.</div>
                     </div>
                     <div class="col-3 col-lg-2">
-                        <select class="form-select" id="gender" onblur="validateInput(this)" required>
+                        <select class="form-select" id="gender" name="Sexo*" onchange="validateInput(this)" required>
                             <option value="" disabled selected hidden>Sexo*</option>
-                            <option value="1">Masculino</option>
-                            <option value="2">Feminino</option>
+                            <option value="Masculino">Masculino</option>
+                            <option value="Feminino">Feminino</option>
                         </select>
                         <div class="invalid-feedback" id="invalid-message-gender">Informe um sexo válido.</div>
                     </div>
@@ -84,15 +78,15 @@
 
                 <div class="row justify-content-center mb-5">
                     <div class="col-4 col-lg-3">
-                        <input type="text" id="address" class="form-control" placeholder="Logradouro*" onblur="validateInput(this)" required>
+                        <input type="text" id="address" name="Logradouro" class="form-control" placeholder="Logradouro*" onblur="validateInput(this)" required>
                         <div class="invalid-feedback" id="invalid-message-address">Informe um logradouro válido.<br> <em>Ex: Rua São Francisco de Sales</em></div>
                     </div>
                     <div class="col-4 col-lg-3">
-                        <input type="text" id="city" class="form-control" placeholder="Cidade*" onblur="validateInput(this)" required>
+                        <input type="text" id="city" name="Cidade" class="form-control" placeholder="Cidade*" onblur="validateInput(this)" required>
                         <div class="invalid-feedback" id="invalid-message-city">Informe uma cidade válida.<br> <em>Ex: Curitiba</em></div>
                     </div>
                     <div class="col-4 col-lg-2">
-                        <select class="form-select" id="state" onblur="validateInput(this)" required>
+                        <select class="form-select" id="state" name="Estado" onchange="validateInput(this)" required>
                             <option value="" disabled selected hidden>Estado*</option>
                             <option value="AC">Acre</option>
                             <option value="AL">Alagoas</option>
@@ -128,15 +122,15 @@
 
                 <div class="row justify-content-center mb-5">
                 <div class="col-4 col-lg-3">
-                        <input type="text" id="houseNumber" class="form-control" placeholder="Número da residência*" onblur="validateInput(this)" required>
+                        <input type="text" id="houseNumber" name="Número de residência*" class="form-control" placeholder="Número da residência" onblur="validateInput(this)" required>
                         <div class="invalid-feedback" id="invalid-message-houseNumber">Informe uma número de residência válido.<br> <em>Ex: 338</em></div>
                     </div>
                     <div class="col-4 col-lg-3">
-                        <input type="text" id="complement" class="form-control" placeholder="Complemento" onblur="validateInput(this)">
+                        <input type="text" id="complement" name="Complemento" class="form-control" placeholder="Complemento" onblur="validateInput(this)">
                         <div class="invalid-feedback" id="invalid-message-complement">Informe um complemento válido.<br> <em>Ex: Sobrado 20</em></div>
                     </div>
                     <div class="col-4 col-lg-2">
-                        <input type="tel" id="cep" maxLength="9" class="form-control" placeholder="CEP*" onblur="validateInput(this)" required>
+                        <input type="tel" id="cep" name="CEP" maxLength="9" class="form-control" placeholder="CEP*" onblur="validateInput(this)" required>
                         <div class="invalid-feedback" id="invalid-message-cep">Informe um CEP válido.<br> <em>Ex: 81720-290</em></div>
                     </div>
                 </div>
