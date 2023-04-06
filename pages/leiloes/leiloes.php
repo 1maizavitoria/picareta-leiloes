33609e9b3bd397c53234b4247cb1afafa284b41c
Matheus <matheus_romeike@sisand.com.br>
2023-04-05 21:03:44 -0300

    ajustando responsividade da tela leiloes

--- pages/leiloes/leiloes.php
+++ pages/leiloes/leiloes.php
@@ -15,37 +15,35 @@
 
     <h2 class="h1-responsive font-weight-bold text-center my-4">Leilões</h2>
 
-    <div class="mb-2">
-        <div class="row mid g-3">
-            <div class="col-7">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Leilão Presencial/Online N.º1</h5>
-                    </div>
-                    <div class="card-body">
-                        <i class='fas fa-map-marker-alt'></i>
-                        <p class='card-text'>Rua Imaculada Conceição, 1155 - Prado Velho, Curitiba</p>
-                        <i class="fas fa-calendar-alt"></i>
-                        <p class='card-text'>18/07/2023</p>
-                        <div class="justify-content-center d-flex">
-                            <button onclick="window.location.href = './../../pages/lotes/lotes.php?id=0'"; class="btn btn-outline-success col-6">Acessar Leilão</button>
-                        </div>
+    <div class="column align-items-center mid">
+        <div class="col-5 mx-auto mb-5">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Leilão Presencial/Online N.º1</h5>
+                </div>
+                <div class="card-body">
+                    <i class='fas fa-map-marker-alt'></i>
+                    <p class='card-text'>Rua Imaculada Conceição, 1155 - Prado Velho, Curitiba</p>
+                    <i class="fas fa-calendar-alt"></i>
+                    <p class='card-text'>18/07/2023</p>
+                    <div class="justify-content-center d-flex">
+                        <button onclick="window.location.href = './../../pages/lotes/lotes.php?id=0'"; class="btn btn-outline-success col-6">Acessar Leilão</button>
                     </div>
                 </div>
             </div>
-            <div class="col-7">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Leilão Presencial/Online N.º2</h5>
-                    </div>
-                    <div class="card-body">
-                        <i class='fas fa-map-marker-alt'></i>
-                        <p class='card-text'>Rua Imaculada Conceição, 1155 - Prado Velho, Curitiba</p>
-                        <i class="fas fa-calendar-alt"></i>
-                        <p class='card-text'>18/07/2023</p>
-                        <div class="justify-content-center d-flex">
-                            <button onclick="window.location.href = './../../pages/lotes/lotes.php?id=0'"; class="btn btn-outline-success col-6">Acessar Leilão</button>
-                        </div>
+        </div>
+        <div class="col-5 mx-auto">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Leilão Presencial/Online N.º2</h5>
+                </div>
+                <div class="card-body">
+                    <i class='fas fa-map-marker-alt'></i>
+                    <p class='card-text'>Rua Imaculada Conceição, 1155 - Prado Velho, Curitiba</p>
+                    <i class="fas fa-calendar-alt"></i>
+                    <p class='card-text'>18/07/2023</p>
+                    <div class="justify-content-center d-flex">
+                        <button onclick="window.location.href = './../../pages/lotes/lotes.php?id=0'"; class="btn btn-outline-success col-6">Acessar Leilão</button>
                     </div>
                 </div>
             </div>
