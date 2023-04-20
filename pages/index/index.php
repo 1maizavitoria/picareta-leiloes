39ca2e829f8c074a04d9e7a4ed199d3db434bc11
abressam <amandabressam@gmail.com>
2023-04-19 22:05:46 -0300

    Finalizando header e montando organização do index

--- pages/index/index.php
+++ pages/index/index.php
@@ -14,17 +14,57 @@
     include './../../components/header/header.html';
     ?>
 
-    <div class="container">
-        <div class="row">
+    <div class="container d-flex flex-column gap-3">
+        <div class="row m-3">
             <div class="col-md-6">
-            <img src="./images/carro1.jpg" width="500" alt="Imagem">
+                <img src="./images/sell.jpg" width="450" alt="Imagem">
             </div>
-            <div class="col-md-6">
-            <h2>Título do Texto</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, nibh eu mattis accumsan, odio enim dapibus sapien, at aliquet ipsum urna quis enim. Mauris scelerisque libero a tellus lacinia, nec elementum libero volutpat.</p>
-            <p>Sed vel ante purus. Nullam imperdiet auctor arcu a lacinia. Ut vel turpis purus. Praesent at eros ac odio lobortis fringilla. Etiam quis urna eu massa varius pharetra. Donec tincidunt velit vel lectus tincidunt, a luctus felis pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin ut elit sit amet elit aliquam eleifend nec in sapien. </p>
+            <div class="col-md-6 mt-2">
+                <h2>Nossa História</h2>
+                <p>
+                    O Picareta Leilões surgiu a partir do trabalho árduo, dedicação e paixão. 
+                    Começou com uma pequena equipe dedicada de funcionários, incluindo um 
+                    leiloeiro experiente e um grupo de mecânicos habilidosos para garantir 
+                    que os carros estivessem em boas condições antes de irem para o leilão. 
+                    Em pouco tempo de mercado, conquistamos a confiança dos clientes, garantindo
+                    qualidade, honestidade e integridade nos negócios.
+                </p>
             </div>
         </div>
+
+        <div class="row m-3">
+            <div class="col-md-6 mr-2">
+                <h2>O que fazemos?</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel velit in leo blandit blandit. Praesent mauris risus, rutrum a pellentesque non, feugiat nec libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse ut justo consectetur, tempor felis et, tincidunt diam. Etiam sit amet molestie neque, cursus pellentesque nisi. Duis at efficitur lorem, vitae faucibus velit. Proin orci neque, iaculis sed orci sed, aliquam scelerisque odio. Duis quis ex vitae risus suscipit hendrerit. Nunc id facilisis ante, at pulvinar ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eleifend nisl tortor, ut cursus diam imperdiet sed. Nulla a dolor ac eros luctus viverra quis et leo.
+                </p>
+            </div>
+
+            <div class="col-md-6">
+                <img src="./images/carro1.jpg" width="500" alt="Imagem">
+            </div>
+        </div>
+
+        <div class="col-md-12 m-3">
+            <h2 class="d-flex justify-content-center">Como funciona</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel velit in leo blandit blandit. Praesent mauris risus, rutrum a pellentesque non, feugiat nec libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse ut justo consectetur, tempor felis et, tincidunt diam. Etiam sit amet molestie neque, cursus pellentesque nisi. Duis at efficitur lorem, vitae faucibus velit. Proin orci neque, iaculis sed orci sed, aliquam scelerisque odio. Duis quis ex vitae risus suscipit hendrerit. Nunc id facilisis ante, at pulvinar ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eleifend nisl tortor, ut cursus diam imperdiet sed. Nulla a dolor ac eros luctus viverra quis et leo.
+            </p>
+        </div>
+
+        <div class="row m-2">
+            <div class="col-md-6 mt-2">
+                <h2>O Leiloeiro</h2>
+                <h4>Alcides Carvalho</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel velit in leo blandit blandit. Praesent mauris risus, rutrum a pellentesque non, feugiat nec libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse ut justo consectetur, tempor felis et, tincidunt diam. Etiam sit amet molestie neque, cursus pellentesque nisi. Duis at efficitur lorem, vitae faucibus velit. Proin orci neque, iaculis sed orci sed, aliquam scelerisque odio. Duis quis ex vitae risus suscipit hendrerit. Nunc id facilisis ante, at pulvinar ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eleifend nisl tortor, ut cursus diam imperdiet sed. Nulla a dolor ac eros luctus viverra quis et leo.
+                </p>
+            </div>
+            <div class="col-md-6 d-flex justify-content-center">
+                <img src="./images/leiloeiro.jpg" width="300" alt="Imagem">
+            </div>
+        </div>
+
     </div>
 
     <?php
