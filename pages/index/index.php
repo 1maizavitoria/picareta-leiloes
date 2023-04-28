bdf90e56f6aa4cf91b49df651757ccd4f081e0e6
Matheus <matheus_romeike@sisand.com.br>
2023-04-28 19:55:04 -0300

    deixando index responsivo

--- pages/index/index.php
+++ pages/index/index.php
@@ -15,11 +15,11 @@
     ?>
 
     <div class="container d-flex flex-column gap-3">
-        <div class="row m-3">
-            <div class="col-md-6">
-                <img src="./images/sell.jpg" width="450" alt="Imagem">
+        <div class="row justify-content-center mt-2">
+            <div class="col-md-9 col-lg-6">
+                <img src="./images/sell.jpg" alt="Imagem">
             </div>
-            <div class="col-md-6 mt-2">
+            <div class="col-md-8 col-lg-6 mt-2">
                 <h2>Nossa História</h2>
                 <p>
                     O Picareta Leilões surgiu a partir do trabalho árduo, dedicação e paixão. 
@@ -32,7 +32,7 @@
             </div>
         </div>
 
-        <div class="row m-3">
+        <div class="row justify-content-center">
             <div class="col-md-6 mr-2">
                 <h2>O que fazemos?</h2>
                 <p>
@@ -40,19 +40,19 @@
                 </p>
             </div>
 
-            <div class="col-md-6">
+            <div class="col-xs-1 col-lg-6">
                 <img src="./images/carro1.jpg" width="500" alt="Imagem">
             </div>
         </div>
 
-        <div class="col-md-12 m-3">
+        <div class="col-md-12">
             <h2 class="d-flex justify-content-center">Como funciona</h2>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel velit in leo blandit blandit. Praesent mauris risus, rutrum a pellentesque non, feugiat nec libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse ut justo consectetur, tempor felis et, tincidunt diam. Etiam sit amet molestie neque, cursus pellentesque nisi. Duis at efficitur lorem, vitae faucibus velit. Proin orci neque, iaculis sed orci sed, aliquam scelerisque odio. Duis quis ex vitae risus suscipit hendrerit. Nunc id facilisis ante, at pulvinar ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eleifend nisl tortor, ut cursus diam imperdiet sed. Nulla a dolor ac eros luctus viverra quis et leo.
             </p>
         </div>
 
-        <div class="row m-2">
+        <div class="row mb-2">
             <div class="col-md-6 mt-2">
                 <h2>O Leiloeiro</h2>
                 <h4>Alcides Carvalho</h4>
@@ -61,7 +61,7 @@
                 </p>
             </div>
             <div class="col-md-6 d-flex justify-content-center">
-                <img src="./images/leiloeiro.jpg" width="300" alt="Imagem">
+                <img src="./images/leiloeiro.jpg" height="600" alt="Imagem">
             </div>
         </div>
 
