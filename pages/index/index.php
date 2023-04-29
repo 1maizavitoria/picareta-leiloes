916ee7a65a060bc6948b7108d8b5790de8c66371
Matheus <matheus_romeike@sisand.com.br>
2023-04-28 22:28:25 -0300

    melhorando um pouco o index

--- pages/index/index.php
+++ pages/index/index.php
@@ -53,14 +53,14 @@
         </div>
 
         <div class="row mb-2">
-            <div class="col-md-6 mt-2">
+            <div class="col-md-5 mt-2">
                 <h2>O Leiloeiro</h2>
                 <h4>Alcides Carvalho</h4>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel velit in leo blandit blandit. Praesent mauris risus, rutrum a pellentesque non, feugiat nec libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse ut justo consectetur, tempor felis et, tincidunt diam. Etiam sit amet molestie neque, cursus pellentesque nisi. Duis at efficitur lorem, vitae faucibus velit. Proin orci neque, iaculis sed orci sed, aliquam scelerisque odio. Duis quis ex vitae risus suscipit hendrerit. Nunc id facilisis ante, at pulvinar ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eleifend nisl tortor, ut cursus diam imperdiet sed. Nulla a dolor ac eros luctus viverra quis et leo.
                 </p>
             </div>
-            <div class="col-md-6 d-flex justify-content-center">
+            <div class="col-md-7 d-flex justify-content-center">
                 <img src="./images/leiloeiro.jpg" height="600" alt="Imagem">
             </div>
         </div>
