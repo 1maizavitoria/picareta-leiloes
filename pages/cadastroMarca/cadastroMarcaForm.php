b938b140c2dfcce0e8196f87c5aca6351da94b17
Guilherme Marcondes <guilherme@sisand.com.br>
2023-05-03 15:21:05 -0300

    finalizando crud de marca

--- pages/cadastroMarca/cadastroMarcaForm.php
+++ pages/cadastroMarca/cadastroMarcaForm.php
@@ -12,10 +12,39 @@
 
     <?php
     include './../../components/header/header.php';
+    include './../../components/toastr/toastr.php';
+
+    $id = $_GET['id'];
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $descricao = $_POST['descricao'];
-    executeQuery("INSERT INTO marca (descricao) VALUES ('$descricao')");
+        $descricao = $_POST['descricao'];
+        if (!empty($descricao)) {
+            
+            if(isset($_POST["adicionar"])) {
+                executeQuery("INSERT INTO marca (descricao) VALUES ('$descricao')");   
+            }
+
+            if(isset($_POST['salvar'])) {
+                executeQuery("UPDATE marca SET descricao = '$descricao' WHERE marcaId = '$id'");
+                toastr('success', 'Modelo atualizado!');
+            }        
+        }
+
+        if (isset($_POST['deletar'])) {
+            $marcaSelecionada = executeQuery("SELECT ma.marcaId, ma.descricao FROM marca ma INNER JOIN modelo mo on mo.marcaId = ma.marcaId WHERE ma.marcaId = '$id'");
+            if($marcaSelecionada -> num_rows > 0) {
+                $marca = $marcaSelecionada -> fetch_assoc();
+            }
+            if(isset($marca)){
+                toastr('error', 'Esta marca possui um modelo vinculado, não é possível excluí-la.');
+
+            }else{
+                executeQuery("DELETE FROM marca WHERE marcaId = '$id'");
+                toastr('success', 'Modelo excluído');
+            }
+        }
+
+        header("Location: http://localhost/picareta-leiloes/pages/cadastroMarca/cadastroMarca.php");
     }
     ?>
     
@@ -38,29 +67,46 @@
 
             <form class="row d-flex justify-content-center" id="form" action="" method="POST">
 
-                <div class="row justify-content-center mb-5">
-                    <div class="col-6 col-lg-4">
-                        <input type="text" id="name" name="descricao" class="form-control" placeholder="Marca*" onblur="validateInput(this)" required>
-                        <div class="invalid-feedback" id="invalid-message-name">Informe um nome de marca válido.<br> <em>Ex: Chevrolet</em></div>
-                    </div>
-                </div>
+               
                 
                 <?php 
                     if (isset($_GET["id"]))
                         $id = $_GET["id"];
 
                         if (isset($id) && $id != "") {
+                            $marcaSelecionada = executeQuery("SELECT ma.marcaId, ma.descricao FROM marca ma WHERE ma.marcaid = '$id'");
+                            if($marcaSelecionada -> num_rows > 0) {
+                                $marca = $marcaSelecionada -> fetch_assoc();
+                            }
+                            if (isset($marca)) {
+                                echo "
+                                <div class='row justify-content-center mb-5'>
+                                <div class='col-6 col-lg-4'>
+                                    <input type='text' id='name' name='descricao' class='form-control' placeholder='Marca*' value='" . $marca["descricao"] . "' onblur='validateInput(this)' required>
+                                    <div class='invalid-feedback' id='invalid-message-name'>Informe um nome de marca válido.<br> <em>Ex: Chevrolet</em></div>
+                                </div>
+                                </div>
+                                ";
+                            }
+
                             echo "
                             <div class='col-6 col-lg-3 mx-auto d-flex justify-content-around'>
-                                <button type='submit' value='deletar' class='btn btn-outline-danger col-5'>Deletar</button>
-                                <button type='submit' value='salvar' class='btn btn-outline-success col-5' onclick=\"checkAllFields('form')\">Salvar</button>
+                                <button type='submit' value='deletar' name='deletar' class='btn btn-outline-danger col-5'>Deletar</button>
+                                <button type='submit' value='salvar' name='salvar' class='btn btn-outline-success col-5' onclick=\"checkAllFields('form')\">Salvar</button>
                             </div>
                             ";
                         } else {
                             echo "
+                            <div class='row justify-content-center mb-5'>
+                            <div class='col-4 col-lg-4'>
+                                <input type='text' id='name' name='descricao' class='form-control' placeholder='Marca*' onblur='validateInput(this)' required>
+                                <div class='invalid-feedback' id='invalid-message-name'>Informe um nome de marca válido.<br> <em>Ex: Chevrolet</em></div>
+                            </div>
+                            </div>
+                            
                             <div class='col-6 col-lg-3 mx-auto d-flex justify-content-around'>
                                 <button type='button' value='cancelar' class='btn btn-outline-danger col-5' onclick=\"window.history.back()\">Cancelar</button>
-                                <button type='submit' value='adicionar' class='btn btn-outline-success col-5' onclick=\"checkAllFields('form')\">Adicionar</button>
+                                <button type='submit' value='adicionar' name='adicionar' class='btn btn-outline-success col-5' onclick=\"checkAllFields('form')\">Adicionar</button>
                             </div>
                             ";
                         }
