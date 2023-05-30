8aee3603216c5cd72b3af5b0f9e9cfc2195850b3
abressam <amandabressam@gmail.com>
2023-05-29 22:49:39 -0300

    Adicionando listagem de veículos

--- pages/cadastroVeiculo/cadastroVeiculo.php
+++ pages/cadastroVeiculo/cadastroVeiculo.php
@@ -24,25 +24,31 @@
         <div class="right">
 
             <div class="d-flex justify-content-center mt-5">
-                <h2>Cadastro de Veiculo</h2>
+                <h2>Cadastro de Veículo</h2>
             </div>
 
             <div class="col-12 px-5 mt-5 d-flex justify-content-center">
                 <?php 
                     include './../../components/grid/grid.php';
-                    $produtos = array(
-                        array("1", "FORD", "FIESTA", "2010", "PRETO", "AAA-0000"),
-                        array("2", "BMW", "X1", "2015", "PRETO", "AAA-0000"),
-                        array("3", "FIAT", "UNO", "2018", "VERMELHO",  "AAA-0000"),
-                        array("4", "VOLKSWAGEN", "GOL", "2019", "PRATA", "AAA-0000"),
-                        array("5", "CHEVROLET", "ONIX", "2018", "BRANCO", "AAA-0000"),
-                    );
+
+                    $veiculos = array();
+                    $selectVeiculo = executeQuery("SELECT ma.descricao AS dsMarca, mo.descricao AS dsModelo, mo.anoModelo, co.descricao AS dsCor, ve.placa FROM veiculo ve 
+                    INNER JOIN modelo mo ON ve.modeloId = mo.modeloId 
+                    INNER JOIN marca ma ON mo.marcaId = ma.marcaId 
+                    INNER JOIN modeloCor mc ON mo.modeloId = mc.modeloId 
+                    INNER JOIN cor co ON mc.corId = co.corId");
+
+                    while($row = mysqli_fetch_assoc($selectVeiculo)) {
+                        echo "<script>console.log(".json_encode($row).");</script>";
+
+                        $veiculos[] = array(NULL, $row['dsMarca'], $row['dsModelo'], $row['anoModelo'], $row['dsCor'], $row['placa']);
+                    }
                 
                     $titulos = array('Marca', 'Modelo', 'Ano Modelo', 'Cor', 'Placa');
                     $editavel = true;
                     $urlClick = "cadastroVeiculoForm.php?id=";
                 
-                    gerarGrid($titulos, $produtos, 10, $editavel, $urlClick);
+                    gerarGrid($titulos, $veiculos, 10, $editavel, $urlClick);
                 ?>
             </div>
 
